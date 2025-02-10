94219a9bd8b1089848dabac1d22bd6237f26b220
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-10 15:34:51 -0700

    Fix typo

--- src/problems/JellybeanWorldBig.py
+++ src/problems/JellybeanWorldBig.py
@@ -5,7 +5,7 @@ from experiment.ExperimentModel import ExperimentModel
 from problems.BaseProblem import BaseProblem
 
 
-class JellyBeanWorldBig(BaseProblem):
+class JellybeanWorldBig(BaseProblem):
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
         super().__init__(exp, idx, collector)
 
