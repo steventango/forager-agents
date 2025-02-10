ee3e4fc968b2d5844aaf05391634d31ef0e4a630
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-10 16:31:52 -0700

    Add scripts for big experiment

---
+++ scripts/big-1-percent-sweep.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# exit script on error
+set -e
+
+python scripts/local.py --runs 5 -e experiments/big/Forager-1-percent-sweep/DQN.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 5 -e experiments/big/JellybeanWorld-1-percent-sweep/DQN.json --entry src/continuing_main.py --cpus 30
+python experiments/forager/learning_curve.py save pdf
