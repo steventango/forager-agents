0b844c303a3c5a2e30a350ad250d726956eb6149
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-18 23:25:39 -0700

    Update script

--- scripts/big.sh
+++ scripts/big.sh
@@ -4,5 +4,5 @@
 set -e
 
 python scripts/local.py --runs 30 -e experiments/big/Forager/DQN.json --entry src/continuing_main.py --cpus 30
-python scripts/local.py --runs 30 -e experiments/big/JellybeanWorld/DQN.json --entry src/continuing_main.py --cpus 30
-python experiments/forager/learning_curve.py save pdf
+# python scripts/local.py --runs 30 -e experiments/big/JellybeanWorld/DQN.json --entry src/continuing_main.py --cpus 30
+python experiments/big/learning_curve.py save pdf
