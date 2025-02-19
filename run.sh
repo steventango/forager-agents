800ebfdc9540b340fb7fcf7b7ea0b54762eb690c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 13:59:17 -0700

    Run evaluation of ForagerTwoBiomeBig

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 bash scripts/forager-two-biome-small-sweep.sh
 bash scripts/forager-two-biome-sweep.sh
 bash scripts/forager-two-biome-large-sweep.sh
+bash scripts/forager-two-biome-large.sh
---
+++ scripts/forager-two-biome-large.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# exit script on error
+set -e
+
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-3.json --entry src/continuing_main.py --cpus 30 # --gpu
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-15.json --entry src/continuing_main.py --cpus 30 # --gpu
+python experiments/forager-two-biome-large/learning_curve.py save pdf
