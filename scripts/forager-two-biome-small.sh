af9864e08bad0e7ca5577f33cd5bf19afb9976db
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 13:20:23 -0700

    Run evaluation on ForagerTwoBiomeSmall

---
+++ scripts/forager-two-biome-small.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# exit script on error
+set -e
+
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-3.json --entry src/continuing_main.py --cpus 30 # --gpu
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-17.json --entry src/continuing_main.py --cpus 30 # --gpu
+python experiments/forager-two-biome-small/learning_curve.py save pdf
