0a31d190b09114d4c8b2d78e3da22a1cdcd3f2cb
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 16:04:58 -0700

    GPU is slower for these configs

--- scripts/forager-two-biome-large.sh
+++ scripts/forager-two-biome-large.sh
@@ -3,9 +3,9 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-3.json --entry src/continuing_main.py --cpus 30 # --gpu
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-5.json --entry src/continuing_main.py --cpus 30 # --gpu
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-7.json --entry src/continuing_main.py --cpus 30 # --gpu
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-9.json --entry src/continuing_main.py --cpus 30 # --gpu
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-15.json --entry src/continuing_main.py --cpus 30 # --gpu
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-3.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-5.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-7.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-9.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-15.json --entry src/continuing_main.py --cpus 30
 python experiments/forager-two-biome-large/learning_curve.py save pdf
