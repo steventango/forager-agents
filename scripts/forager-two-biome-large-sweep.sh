745cb199e6bca9510a80da7c82017d5bc8104b8c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 14:32:27 -0700

    Run 10% sweep with more aperture sizes on ForagerTwoBiomeLarge

--- scripts/forager-two-biome-large-sweep.sh
+++ scripts/forager-two-biome-large-sweep.sh
@@ -4,5 +4,8 @@
 set -e
 
 python scripts/local.py --runs 5 -e experiments/forager-two-biome-large-sweep/ForagerTwoBiomeLarge/DQN-3.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 5 -e experiments/forager-two-biome-large-sweep/ForagerTwoBiomeLarge/DQN-5.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 5 -e experiments/forager-two-biome-large-sweep/ForagerTwoBiomeLarge/DQN-7.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 5 -e experiments/forager-two-biome-large-sweep/ForagerTwoBiomeLarge/DQN-9.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 5 -e experiments/forager-two-biome-large-sweep/ForagerTwoBiomeLarge/DQN-15.json --entry src/continuing_main.py --cpus 30
 python experiments/forager-two-biome-large-sweep/learning_curve.py save pdf
