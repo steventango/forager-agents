a8fb24d85fc6cdb6d29d0b21c4de203124a551a8
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 17:42:35 -0700

    Remove gpu argument

--- scripts/forager-two-biome-small.sh
+++ scripts/forager-two-biome-small.sh
@@ -3,6 +3,6 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-3.json --entry src/continuing_main.py --cpus 30 # --gpu
-python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-17.json --entry src/continuing_main.py --cpus 30 # --gpu
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-3.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-17.json --entry src/continuing_main.py --cpus 30
 python experiments/forager-two-biome-small/learning_curve.py save pdf
