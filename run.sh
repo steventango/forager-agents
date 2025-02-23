cba0b25608da7848c1365912796f1d20ec774742
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-23 14:49:26 -0700

    Configure sweeps

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-bash scripts/forager-two-biome-small-sweep.sh
-bash scripts/forager-two-biome-large-sweep.sh
-bash scripts/forager-two-biome-small.sh
-bash scripts/forager-two-biome-large.sh
+bash scripts/forager-extra-large-sweep.sh
+bash scripts/forager-extra-large.sh
+bash scripts/jellybean-world-extra-large-sweep.sh
+bash scripts/jellybean-world-extra-large.sh
