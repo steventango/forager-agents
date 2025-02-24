2c8a63c19fdb195758d0d0fb1bb42a3eee59dfe1
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-24 14:49:41 -0700

    Add Random agent

--- scripts/forager-extra-large.sh
+++ scripts/forager-extra-large.sh
@@ -3,6 +3,7 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 30 -e experiments/forager-extra-large/ForagerExtraLarge/DQN.json --entry src/continuing_main.py --cpus 30 --gpu
+python scripts/local.py --runs 30 -e experiments/forager-extra-large/ForagerExtraLarge/Random.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 30 -e experiments/forager-extra-large/ForagerExtraLarge/DQN.json --entry src/continuing_main.py --cpus 30
 # ReferenceError: underlying object has vanished, caused by NumPyRandomGeneratorType
 NUMBA_DISABLE_JIT=1 python experiments/forager-extra-large/learning_curve.py save pdf
