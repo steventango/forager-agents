bf206fb9f76c686f762159c10daa7195b654390b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-24 13:14:17 -0700

    Add Random agent to ForagerTwoBiome

--- scripts/forager-two-biome-large.sh
+++ scripts/forager-two-biome-large.sh
@@ -3,6 +3,7 @@
 # exit script on error
 set -e
 
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/Random.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-3.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-15.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 30 -e experiments/forager-two-biome-large/ForagerTwoBiomeLarge/DQN-7.json --entry src/continuing_main.py --cpus 30
--- scripts/forager-two-biome-small.sh
+++ scripts/forager-two-biome-small.sh
@@ -3,6 +3,7 @@
 # exit script on error
 set -e
 
+python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/Random.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-3.json --entry src/continuing_main.py --cpus 30
 python scripts/local.py --runs 30 -e experiments/forager-two-biome-small/ForagerTwoBiomeSmall/DQN-17.json --entry src/continuing_main.py --cpus 30
 # ReferenceError: underlying object has vanished, caused by NumPyRandomGeneratorType
