4a2db6bce6762f4af266526d11293f5a047d22df
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-24 20:34:29 -0700

    Update JBW configs

---
+++ scripts/jellybean-world-extra-large-slurm.sh
@@ -0,0 +1,1 @@
+python scripts/slurm.py --cluster clusters/cedar.json --runs 1 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/Random.json --entry src/continuing_main.py
--- scripts/jellybean-world-extra-large-sweep-slurm.sh
+++
@@ -1,1 +0,0 @@
-python scripts/slurm.py --cluster clusters/cedar.json --runs 1 -e experiments/jellybean-world-extra-larg-sweep/JellybeanWorldExtraLarge/Random.json --entry src/continuing_main.py
