616a9851b9999043416e6a22e2fd0a25e6ef517f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-24 22:14:30 -0700

    Remove PyFixedReps

--- src/algorithms/registry.py
+++ src/algorithms/registry.py
@@ -5,21 +5,12 @@ from algorithms.RandomAgent import RandomAgent
 from algorithms.nn.DQN import DQN
 from algorithms.nn.EQRC import EQRC
 
-from algorithms.tc.ESARSA import ESARSA
-from algorithms.tc.SoftmaxAC import SoftmaxAC
-
 def getAgent(name) -> Type[BaseAgent]:
     if name.startswith('DQN'):
         return DQN
 
     if name == 'EQRC':
         return EQRC
-
-    if name == 'ESARSA':
-        return ESARSA
-
-    if name == 'SoftmaxAC':
-        return SoftmaxAC
 
     if name == 'Random':
         return RandomAgent
