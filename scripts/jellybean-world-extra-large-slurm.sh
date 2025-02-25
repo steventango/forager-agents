198ead9fc9dfc5ec5b7e3433c8c75b0a2ec9c691
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-24 22:38:46 -0700

    Update Slurm script

--- scripts/jellybean-world-extra-large-slurm.sh
+++ scripts/jellybean-world-extra-large-slurm.sh
@@ -1,1 +1,2 @@
 python scripts/slurm.py --cluster clusters/cedar_t1_c4_m2000.json --runs 1 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/Random.json --entry src/continuing_main.py
+python scripts/slurm.py --cluster clusters/cedar_t1_c4_m2000.json --runs 1 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/DQN.json --entry src/continuing_main.py
