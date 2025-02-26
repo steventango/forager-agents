57cf397820180f304fa3f8a80ee251138959b44e
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-25 19:32:18 -0700

    Use GPU for SAC

--- scripts/jellybean-world-extra-large-sweep.sh
+++ scripts/jellybean-world-extra-large-sweep.sh
@@ -3,7 +3,7 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 5 -e experiments/jellybean-world-extra-large-sweep/JellybeanWorldExtraLarge/DQN.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 5 -e experiments/jellybean-world-extra-large-sweep/JellybeanWorldExtraLarge/DQN.json --entry src/continuing_main.py --cpus 30 --gpu
 python scripts/local.py --runs 5 -e experiments/jellybean-world-extra-large-sweep/JellybeanWorldExtraLarge/SAC.json --entry src/continuing_main.py --cpus 30
 # ReferenceError: underlying object has vanished, caused by NumPyRandomGeneratorType
 NUMBA_DISABLE_JIT=1 python experiments/jellybean-world-extra-large-sweep/learning_curve.py save pdf
