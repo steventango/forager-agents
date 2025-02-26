9f01b5a9d1214c47a8daa4977bddf960971ed7df
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-25 17:06:45 -0700

    Request more time and double RAM for JBW

--- scripts/jellybean-world-extra-large-slurm.sh
+++ scripts/jellybean-world-extra-large-slurm.sh
@@ -1,2 +1,2 @@
 python scripts/slurm.py --cluster clusters/cedar_t3_c1_m2000_s4.json --runs 30 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/Random.json --entry src/continuing_main.py
-python scripts/slurm.py --cluster clusters/cedar_t8_c1_m2000_s1.json --runs 30 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/DQN.json --entry src/continuing_main.py
+python scripts/slurm.py --cluster clusters/cedar_t12_c1_m4000_s1.json --runs 30 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/DQN.json --entry src/continuing_main.py
