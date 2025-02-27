0ea14d23a46447ef543fd3bf11f70d8a948b6b17
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-26 22:33:40 -0700

    24GB of RAM for JBW

--- scripts/jellybean-world-extra-large-slurm.sh
+++ scripts/jellybean-world-extra-large-slurm.sh
@@ -1,1 +1,1 @@
-python scripts/slurm_gpu.py --cluster clusters/cedar_g_t18_c1_m4000_s1.json --runs 5 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/SAC.json --entry src/continuing_main.py
+python scripts/slurm_gpu.py --cluster clusters/cedar_g_t18_c1_m24000_s1.json --runs 5 -e experiments/jellybean-world-extra-large/JellybeanWorldExtraLarge/SAC.json --entry src/continuing_main.py
