d70682d04696c7d877b7d578897ef17f84e87224
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-26 22:13:29 -0700

    Run in parallel

--- scripts/forager-temperature.sh
+++ scripts/forager-temperature.sh
@@ -3,8 +3,9 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/Greedy.json --entry src/continuing_main.py --cpus 30
-python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/Random.json --entry src/continuing_main.py --cpus 30
-python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/DQN.json --entry src/continuing_main.py --cpus 30
+python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/Greedy.json --entry src/continuing_main.py --cpus 1 &
+python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/Random.json --entry src/continuing_main.py --cpus 1 &
+python scripts/local.py --runs 1 -e experiments/forager-temperature/ForagerTemperature/DQN.json --entry src/continuing_main.py --cpus 1 &
+wait
 # ReferenceError: underlying object has vanished, caused by NumPyRandomGeneratorType
 NUMBA_DISABLE_JIT=1 python experiments/forager-temperature/learning_curve.py save pdf
